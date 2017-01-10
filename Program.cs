ae55ff22706a590236e50853fa8a5d71165ad243
Ranga Vadlamudi <rangv@microsoft.com>
2017-01-10 11:28:07 -0600

    Initial Code
    Initial Code

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Azure.Devices;
+using Microsoft.Azure.Devices.Common.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace azure_iothub_device_creation_dotnet
+{
+    class Program
+    {
+        static RegistryManager registryManager;
+        //static string connectionString = "{iot hub connection string}";
+        static string connectionString = "HostName=rviothub12052016.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=oP1b7+WTNJFgR+ar6fVWzBidp7/Kn8xCbcKPFFBMYKE=";
+
+        static void Main(string[] args)
+        {
+            registryManager = RegistryManager.CreateFromConnectionString(connectionString);
+            AddDeviceAsync().Wait();
+            Console.ReadLine();
+        }
+
+        private static async Task AddDeviceAsync()
+        {
+            string deviceId = "myDotnetDevice";
+            Device device;
+            try
+            {
+                device = await registryManager.AddDeviceAsync(new Device(deviceId));
+            }
+            catch (DeviceAlreadyExistsException)
+            {
+                device = await registryManager.GetDeviceAsync(deviceId);
+            }
+            Console.WriteLine("Generated device key: {0}", device.Authentication.SymmetricKey.PrimaryKey);
+        }
+    }
+}
